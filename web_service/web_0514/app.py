24223a8873199e13d7fb83c426a49356a82e519e
Seonb2 <162686015+do-po@users.noreply.github.com>
2024-05-14 16:04:37 +0900

    updates

--- web_service/web_0514/app.py
+++ web_service/web_0514/app.py
@@ -1,7 +1,8 @@
 # flask 프레임 워크 안의 특정 기능들을 로드
 # flask, html을 text로 받아오는 render_template, request, 페이지 이동하는 redirect
+    # url_for는 외부 데이터 경로 지정하는 것
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 # mysql과 연동을 하는 라이브러리 로드
 
@@ -72,11 +73,11 @@ def sign_in():
     # 예외가 발생
 
     except: # 처음 로그인을 하는 경우
-        _state = 1
+        _state = "1"
 
     # log_in.html과 state를 되돌려준다.
     
-    return render_template('login.html', state = _state)
+    return render_template('log_in.html', state = _state)
 
 # 로그인 화면에서 id, password 데이터를 보내는 api 생성
 
@@ -117,12 +118,113 @@ def main():
 
     if db_result:
         return render_template('main.html')
+        # frontend 아직 안 만들었는데 테스트 하고 싶은 경우
+        # 위의 return 주석 처리 하고 아래 결과 나오는 지 확인하면 됨
+        # return 'Login OK'
 
     # 로그인 실패 경우
-        # login 화면인 '/' 으로 되돌아간다
+        # login 화면인 '/' 으로 되돌아가면서 state 변수의 값을 1이 아닌 수로 설정 후 보내준다.
 
     else:
         return redirect('/?state=2')
+        # return 'Login Fail'
+
+# 회원 가입 화면을 보여주는 api 생성
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html')
+
+# id 존재 유무를 판단하는 api
+
+@app.route('/check_id', methods = ['post'])
+def check_id():
+    # front-end에서 비동기 통신(ajax)으로 보내는 id 값을 변수에 저장
+    _id = request.form['input_id']
+    
+    # 유저에게 받은 데이터 확인 (로그)
+
+    print(f"/check_id[post] -> 유저 id : {_id}")
+
+    # 유저가 보낸 id 값이 사용 가능한가?
+        # 조건문 == 해당하는 id로 table에 데이터가 존재하는가 ?
+
+    check_id_query = """
+        select
+        *
+        from
+        user
+        where
+        id = %s
+    """
+
+    db_result = db_execute(check_id_query, _id)
+
+    # id가 사용 가능한 경우 == db_result 값이 없을 때
+
+    if db_result:
+        result = "0"
+
+    else:
+        result = "1"
+
+    return result
+
+# 회원 정보를 받아서 DB에 삽입을 하는 api
+
+@app.route('/sign_up2', methods = ['post'])
+def sign_up2():
+    # 유저가 보낸 데이터를 변수에 저장
+
+
+    _id = request.form['input_id']
+    _pass = request.form['input_pass']
+    _name = request.form['input_name']
+
+    # 유저가 보낸 데이터 확인 (로그)
+
+    print(f'/sign_up2[post] 유저 id : {_id}, password : {_pass}, 이름 : {_name}')
+
+    # DB에 삽입하는 query 작성
+
+    user_data_query = """
+        insert
+        into
+        `user`
+        values (%s, %s, %s)
+    """
+
+    # 함수 호출 [에러가 발생하는 경우가 있으니 (ID 삽입할 때 다른 사람이랑 하필 겹쳐서 안 들어가지거나 서버 오류거나 등등) try 생성]
+
+    try:
+        db_result = db_execute(user_data_query, _id, _pass, _name)
+        print(db_result) # 이거 사실 필요 없긴한데 (어차피 Query Done 나오니까) 잘 들어갔는 지 확인하려고 넣는 구문
+
+    except:
+        db_result = "3" # 왜 log_in으로 가려고 하지 ?? sign_up으로 가는게 더 좋지 않을까 싶은데 음... 그래야 다시 id 넣어서 회원가입 시도할텐데 말야. log_in으로 가면 다시 sign_up으로 가는 버튼 눌러야 되잖아
+
+    # 로그인 화면으로 되돌아간다
+
+    if db_result == "3":
+        return redirect(f'/?state={db_result}')
+    else:
+        return redirect('/')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # 웹 서버를 실행
 
